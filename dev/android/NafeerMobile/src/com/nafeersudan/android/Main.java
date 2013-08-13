b307a048d4ec4a140c5cbb435dd438c4c77de153
unknown <Nadir@Pegasus.(none)>
2013-08-13 04:09:08 +0300

    KSoap in working order

--- dev/android/NafeerMobile/src/com/nafeersudan/android/Main.java
+++ dev/android/NafeerMobile/src/com/nafeersudan/android/Main.java
@@ -1,11 +1,15 @@
 package com.nafeersudan.android;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.nafeersudan.android.R;
 
 import com.nafeersudan.android.soap.KSoapHandler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 
 public class Main extends Activity {
@@ -15,9 +19,14 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         KSoapHandler handler = new KSoapHandler("http://www.nafeersudan.com/nafeer_portal/ws/index.php");
-        String response = handler.login("altaif.bakri@gmail.com", "122");
-      //  String response =handler.loadStates();
+        //String response = handler.login("altaif.bakri@gmail.com", "122");
+        ArrayList<String> response =handler.loadAreas("4");
         EditText editText = (EditText) findViewById(R.id.editText1);
-        editText.setText(response);
+        editText.setText(response.get(1));
+        
+        handler.saveNotification("13.08.2013", "nadir", "0912345678", "it", "1", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "no");
+        Log.e("save", "DONE");
     }
 }
+
+
--- dev/android/NafeerMobile/src/com/nafeersudan/android/soap/KSoapHandler.java
+++ dev/android/NafeerMobile/src/com/nafeersudan/android/soap/KSoapHandler.java
@@ -4,6 +4,7 @@
 package com.nafeersudan.android.soap;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -17,86 +18,130 @@ import org.ksoap2.transport.HttpTransportSE;
 import android.util.Log;
 import android.widget.Toast;
 
-/**
- * @author Nadir
- *
- */
 public class KSoapHandler {
 	static String NAMESPACE = "urn:NafeerWebService";
 	private String url;
 	String TAG = "KSoapHandler";
+	private final String ROW_SPLITTER = ",";
+	private final String FIELD_SPLITTER = "-";
 
 	public KSoapHandler(String url)
 	{
 		this.url = url;
 	}
 
-	
-	public String login(String userName, String password)
+
+	public boolean login(String userName, String password)
 	{
-	    String methodName = "login";
-//		String soapAction = NAMESPACE + "#" + methodName;
-//	    //////////////////////////////////////////////////////
-//
-//		SoapObject request = new SoapObject(NAMESPACE, methodName);
-//		PropertyInfo userNamePi = new PropertyInfo();
-//		userNamePi.setName("email");
-//		userNamePi.setValue(userName);
-//		userNamePi.setType(String.class);
-//		PropertyInfo passwordPi = new PropertyInfo();
-//		passwordPi.setValue(password);
-//		passwordPi.setName("password");
-//		passwordPi.setType(String.class);
-//		request.addProperty(userNamePi);
-//		request.addProperty(passwordPi);
-//		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
-//				SoapEnvelope.VER11);
-//		String stringResponse = "Error";
-//		envelope.setOutputSoapObject(request);
-//		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-//		//androidHttpTransport.debug = true;
-//		try {
-//			androidHttpTransport.call(soapAction, envelope);
-//			SoapObject response = (SoapObject) envelope.bodyIn;
-//			stringResponse = response.getProperty(0).toString();			
-//		} catch (Exception e) {
-//			Log.e(TAG, e.getMessage());
-//			//Log.d(TAG, androidHttpTransport.responseDump);
-//			//e.printStackTrace();
-//		}
-//		
-//		
-//		return stringResponse;
-//	}
-//
+		String methodName = "login";
+		//		String soapAction = NAMESPACE + "#" + methodName;
+		//	    //////////////////////////////////////////////////////
+		//
+		//		SoapObject request = new SoapObject(NAMESPACE, methodName);
+		//		PropertyInfo userNamePi = new PropertyInfo();
+		//		userNamePi.setName("email");
+		//		userNamePi.setValue(userName);
+		//		userNamePi.setType(String.class);
+		//		PropertyInfo passwordPi = new PropertyInfo();
+		//		passwordPi.setValue(password);
+		//		passwordPi.setName("password");
+		//		passwordPi.setType(String.class);
+		//		request.addProperty(userNamePi);
+		//		request.addProperty(passwordPi);
+		//		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+		//				SoapEnvelope.VER11);
+		//		String stringResponse = "Error";
+		//		envelope.setOutputSoapObject(request);
+		//		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+		//		//androidHttpTransport.debug = true;
+		//		try {
+		//			androidHttpTransport.call(soapAction, envelope);
+		//			SoapObject response = (SoapObject) envelope.bodyIn;
+		//			stringResponse = response.getProperty(0).toString();			
+		//		} catch (Exception e) {
+		//			Log.e(TAG, e.getMessage());
+		//			//Log.d(TAG, androidHttpTransport.responseDump);
+		//			//e.printStackTrace();
+		//		}
+		//		
+		//		
+		//		return stringResponse;
+		//	}
+		//
 		HashMap<String, String> credintials = new HashMap<String, String>();
 		credintials.put("password", password);
 		credintials.put("email", userName);
-		return processRequest(methodName, credintials);
+		if(processRequest(methodName, credintials).equalsIgnoreCase("1"))
+			return true;
+
+		return false;
+		
 	}
-	
-	public String loadStates()
+//	public String loadStates()
+//	{
+//		String methodName = "loadStates";
+//		String soapAction = "urn:NafeerWebService#loadStateName";
+//		HashMap<String, String> args = new HashMap<String, String>();
+//		String response = processRequest(methodName, null,soapAction );
+//		String[] rows = response.split(ROW_SPLITTER)
+//		return response;
+//	}
+	public HashMap<String, String> loadStates()
 	{
 		String methodName = "loadStates";
 		HashMap<String, String> args = new HashMap<String, String>();
-		return processRequest(methodName, args);
+		String response = processRequest(methodName, args);
+		HashMap<String, String> ret = new HashMap<String, String>();
+
+		if(!response.equalsIgnoreCase("Error"))
+		{
+			String[] states = response.split(ROW_SPLITTER);
+			for(int i = 0; i < states.length; i++)
+			{
+				ret.put(states[i].split(FIELD_SPLITTER)[1],
+						states[i].split(FIELD_SPLITTER)[0]);
+
+			}
+			return ret;
+		}	
+		return null;
 	}
-	
-	public String loadAreas(String stateId)
+
+	public ArrayList<String> loadAreas(String stateId)
 	{
 		String methodName = "loadAreas";
 		HashMap<String, String> args = new HashMap<String, String>();
 		args.put("stateID", stateId);
-		return processRequest(methodName, args);
+		String response = processRequest(methodName, args);
+		ArrayList<String> ret = new ArrayList<String>();
+
+		if(!response.equalsIgnoreCase("Error"))
+		{
+			String[] areas = response.split(ROW_SPLITTER);
+			for(int i = 0; i < areas.length; i++)
+			{
+//				ret.put(areas[i].split(FIELD_SPLITTER)[0],
+//						areas[i].split(FIELD_SPLITTER)[1]);
+				ret.add(areas[i]);
+
+			}
+			return ret;
+		}	
+
+
+		return null;
+
+
+		//return processRequest(methodName, args);
 	}
-	
-	public String saveNotification(String date, String creator, String tell, String occupation,
+
+	public boolean saveNotification(String date, String creator, String tell, String occupation,
 			String areaId, String locationId, String pepNum, String womNum, String chiNum,
 			String oldNum, String deathNum, String injNum, String compDestHuose, 
 			String parDestHouse, String torrHouse, String stockHouse, String elecProblem,
 			String damageRoad, String emergencyCase, String closeDrainage, String other )
 	{
-		String methodName = "saveNotifiaction";
+		String methodName = "saveNotification";
 		HashMap<String, String> args = new HashMap<String, String>();
 		args.put("notification_date",date);
 		args.put("notification_creator",creator);
@@ -119,40 +164,42 @@ public class KSoapHandler {
 		args.put("notification_emergency_case",emergencyCase);
 		args.put("notification_close_drainage",closeDrainage);
 		args.put("notification_other",other);
-		
-		return processRequest(methodName, args);
+
+		String response =  processRequest(methodName, args);
+		if(response.equalsIgnoreCase("1"))
+			return true;
+		return false;
 
 	}
-	
-//	public String loadAreas()
-//	{
-//	    String methodName = "loadAreas";
-//		String soapAction = NAMESPACE + "#" + methodName;
-//	    //////////////////////////////////////////////////////
-//
-//		SoapObject request = new SoapObject(NAMESPACE, methodName);
-//		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
-//				SoapEnvelope.VER11);
-//		String stringResponse="Error";
-//		envelope.setOutputSoapObject(request);
-//		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-//		//androidHttpTransport.debug = true;
-//		try {
-//			androidHttpTransport.call(soapAction, envelope);
-//			SoapObject response = (SoapObject) envelope.bodyIn;
-//			stringResponse = response.getProperty(0).toString();			
-//		} catch (Exception e) {
-//			Log.e(TAG, e.getMessage());
-//			//Log.d(TAG, androidHttpTransport.responseDump);
-//			//e.printStackTrace();
-//		}
-//	}
-	//////////////////////////////////////////////////////////////////////////////////////
 
-	public String processRequest(String methodName, HashMap<String, String> properties)
+	//	public String loadAreas()
+	//	{
+	//	    String methodName = "Areas";
+	//		String soapAction = NAMESPACE + "#" + methodName;
+	//	    //////////////////////////////////////////////////////
+	//
+	//		SoapObject request = new SoapObject(NAMESPACE, methodName);
+	//		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+	//				SoapEnvelope.VER11);
+	//		String stringResponse="Error";
+	//		envelope.setOutputSoapObject(request);
+	//		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+	//		//androidHttpTransport.debug = true;
+	//		try {
+	//			androidHttpTransport.call(soapAction, envelope);
+	//			SoapObject response = (SoapObject) envelope.bodyIn;
+	//			stringResponse = response.getProperty(0).toString();			
+	//		} catch (Exception e) {
+	//			Log.e(TAG, e.getMessage());
+	//			//Log.d(TAG, androidHttpTransport.responseDump);
+	//			//e.printStackTrace();
+	//		}
+	//	}
+	//////////////////////////////////////////////////////////////////////////////////////
+	public String processRequest(String methodName, HashMap<String, String> properties, String soapAction)
 	{
-		String soapAction = NAMESPACE + "#" + methodName;
-	    //////////////////////////////////////////////////////
+		//String soapAction = NAMESPACE + "#" + ;
+		//////////////////////////////////////////////////////
 
 		SoapObject request = new SoapObject(NAMESPACE, methodName);
 		if (properties != null)
@@ -170,6 +217,7 @@ public class KSoapHandler {
 		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
 				SoapEnvelope.VER11);
 		String stringResponse = "Error";
+		Log.e("tst", "Starting this thing");
 		Log.d(TAG, request.toString());
 		envelope.setOutputSoapObject(request);
 		HttpTransportSE androidHttpTransport = new HttpTransportSE(url);
@@ -180,6 +228,45 @@ public class KSoapHandler {
 			stringResponse = response.getProperty(0).toString();			
 		} catch (Exception e) {
 			Log.e(TAG, e.getMessage());
+			Log.e("ksoapprob",e.toString());
+			//Log.d(TAG, androidHttpTransport.responseDump);
+			//e.printStackTrace();
+		}		
+		return stringResponse;
+	}
+	public String processRequest(String methodName, HashMap<String, String> properties)
+	{
+		String soapAction = NAMESPACE + "#" + methodName;
+		//////////////////////////////////////////////////////
+
+		SoapObject request = new SoapObject(NAMESPACE, methodName);
+		if (properties != null)
+		{
+			PropertyInfo pi;
+			Iterator it = properties.entrySet().iterator();
+			while (it.hasNext()) {
+				pi = new PropertyInfo();
+				HashMap.Entry<String,String> pair = (HashMap.Entry<String,String>)it.next();
+				pi.setName(pair.getKey());
+				pi.setValue(pair.getValue());
+				request.addProperty(pi);
+			}
+		}
+		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+				SoapEnvelope.VER11);
+		String stringResponse = "Error";
+		Log.e("tst", "Starting this thing");
+		Log.d(TAG, request.toString());
+		envelope.setOutputSoapObject(request);
+		HttpTransportSE androidHttpTransport = new HttpTransportSE(url);
+		//androidHttpTransport.debug = true;
+		try {
+			androidHttpTransport.call(soapAction, envelope);
+			SoapObject response = (SoapObject) envelope.bodyIn;
+			stringResponse = response.getProperty(0).toString();			
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+			Log.e("ksoapprob",e.getMessage());
 			//Log.d(TAG, androidHttpTransport.responseDump);
 			//e.printStackTrace();
 		}		
